e272955b3245104eaeadf9f48a2bf39b7af70ee7
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-03-30 23:35:44 +0530

    mail message

---
+++ src/Components/MessageMail.jsx
@@ -0,0 +1,40 @@
+import { Box, Typography } from "@mui/material";
+
+function MessageMail({ message }) {
+  return (
+    <Box
+      sx={{
+        width: "753px",
+        height: "236px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
+        margin: "auto",
+      }}
+    >
+      {/* <p>{JSON.stringify(message)}</p> */}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <Typography>{message.subject}</Typography>
+          <Typography>{message.time}</Typography>
+        </Box>
+        <Typography>{message.from}</Typography>
+        <Typography>{message.to}</Typography>
+      </Box>
+      <Typography>{message.intro}</Typography>
+      <Typography>{message.message}</Typography>
+    </Box>
+  );
+}
+
+export default MessageMail;
