dc4c455773959c13f674b126dd8fce658c0781a7
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-03-30 23:33:53 +0530

    store folder rename, assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 import { BrowserRouter } from "react-router-dom";
 import MainRoute from "./MainRoute/MainRoute";
 import { Provider } from "react-redux";
-import store from "./Strore/Store";
-import { ChakraProvider } from "@chakra-ui/react";
+import store from "./Store/Store";
 import NavBar from "./Components/NavBar";
-
+import { Paper, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+const theme = createTheme({
+  palette: {
+    type: "dark",
+  },
+});
+// npm install @mui/material @emotion/react @emotion/styled
 function App() {
   return (
     <>
-      <ChakraProvider>
-        <Provider store={store}>
-          <BrowserRouter>
-            {/* <NavBar /> */}
-            <MainRoute />
-          </BrowserRouter>
-        </Provider>
-      </ChakraProvider>
+      {/* <ChakraProvider> */}
+      <ThemeProvider theme={theme}>
+        <Paper>
+          <Provider store={store}>
+            <BrowserRouter>
+              {/* <NavBar /> */}
+              <MainRoute />
+            </BrowserRouter>
+          </Provider>
+        </Paper>
+      </ThemeProvider>
+      {/* </ChakraProvider> */}
     </>
   );
 }
