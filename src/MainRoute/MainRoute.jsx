5941bb5322b700f3dda5d39a035a6e69ce664ef9
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-03-30 11:38:17 +0530

    mainroute

--- src/MainRoute/MainRoute.jsx
+++ src/MainRoute/MainRoute.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
 import Home from "../Pages/Home";
+import Login from "../Pages/Login";
 
 function MainRoute() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
