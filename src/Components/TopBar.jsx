57b9f9ec5e6768b71880edf7929c5a179e44dea6
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-03-30 23:39:10 +0530

    top bar

--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -1,17 +1,43 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Typography, Box } from "@mui/material";
 
 function TopBar() {
   return (
     <>
-      <Box
+      {/* <Box
+        sx={{
+          backgoundColor: "#343A40",
+          display: "flex",
+          color: "white",
+          padding: "10px 20px",
+          justifyContent: "space-between",
+        }}
         bg={"#343A40"}
         className="text-white flex items-center px-10 justify-between w-full h-16 "
       >
-        <Text>OneBox</Text>
+        <Typography>OneBox</Typography>
         <Box>
           <Box></Box>
           <Box>
-            <Text>Tim's WorkSpace</Text>
+            <Typography>Tim's WorkSpace </Typography> 
+          </Box>
+        </Box>
+      </Box> */}
+      <Box
+        sx={{
+          backgroundColor: "#343A40",
+          display: "flex",
+          color: "white",
+          padding: "10px 20px",
+          justifyContent: "space-between",
+          alignItems: "center",
+          height: "64px",
+        }}
+      >
+        <Typography>OneBox</Typography>
+        <Box>
+          <Box></Box>
+          <Box>
+            <Typography>Tim's WorkSpace</Typography>
           </Box>
         </Box>
       </Box>
