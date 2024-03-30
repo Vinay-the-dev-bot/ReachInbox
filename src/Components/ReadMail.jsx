b5899da99f4843265b655682741b0a87f9fb8cd8
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-03-30 23:36:06 +0530

    read a  mail

---
+++ src/Components/ReadMail.jsx
@@ -0,0 +1,54 @@
+import { Box, Typography } from "@mui/material";
+import MessageMail from "./MessageMail";
+
+function ReadMail() {
+  const message = {
+    subject: "New Product Launch",
+    time: "20 june 2022 : 9:16AM",
+    // from : jeanne@icloud.com cc : lennon.j@mail.com
+    from: "from : jeanne@icloud.com cc : lennon.j@mail.com",
+    cc: "to : lennon.j@mail.com",
+    intro: "Hi soajddcfsojdnjf",
+    message:
+      "Hi {FIRST_NAME} I would like to introduce you to SaaSgrow, a productized design service specifically tailored for saas startups. Our aim is to help you enhance the user experience and boost the visual appeal of your software products.",
+  };
+  return (
+    <>
+      <Box>
+        <Box
+          sx={{
+            width: "799px",
+            height: "70px",
+            top: "69px",
+            left: "363px",
+            padding: "0px 8px 0px 17px",
+            gap: "8px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            borderRadius: "7px 7px 0px 0px",
+            border: "0px solid transparent",
+            borderBottom: "1px solid #101113",
+          }}
+        >
+          <Box
+            sx={{
+              width: "422px",
+              height: "46px",
+            }}
+          >
+            <Typography>Orlando</Typography>
+            <Typography>Orlando@gmail.com</Typography>
+          </Box>
+          <Box>Meeting Completed DRPDWN</Box>
+          <Box>Move DRPDWN</Box>
+          <Box>Three Dots</Box>
+        </Box>
+        <Typography>TODAY</Typography>
+        <MessageMail message={message} />
+      </Box>
+    </>
+  );
+}
+
+export default ReadMail;
