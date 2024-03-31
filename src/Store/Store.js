cf0d88700692ef12aef2e74b4a72623b353d71df
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-03-31 23:55:38 +0530

    theme

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -2,12 +2,15 @@ import { createStore } from "redux";
 
 const initialState = {
   isLoggedIn: "",
+  darkMode: true,
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "LOGIN":
       return { ...state, isLoggedIn: true };
+    case "THEME":
+      return { ...state, darkMode: !state.darkMode };
 
     default:
       return state;
