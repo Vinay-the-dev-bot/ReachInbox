952803cd577d26385bf9a02e9a2445c48665f216
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-03-31 13:30:27 +0530

    navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,13 +1,13 @@
-import { Box } from "@chakra-ui/react";
+import { Box } from "@mui/material";
 import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <>
       <Box
-        margin={"50px auto"}
-        display={"flex"}
-        justifyContent={"space-around"}
+      // margin={"50px auto"}
+      // display={"flex"}
+      // justifyContent={"space-around"}
       >
         <Link to="/">HOME</Link>
         <Link to="/login">LOGIN</Link>
